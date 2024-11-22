1dd1b336497bc83e69fb119151896591209444fa
Not A Bird <74840366+Not-A-Bird-07@users.noreply.github.com>
2024-11-22 13:47:25 -0800

    wrong folder

--- Gorilla Tag Mod Template/Gorilla Tag Mod Template/Plugin Info.cs
+++
@@ -1,9 +0,0 @@
-namespace Gorilla_Tag_Mod_Template
-{
-    public class Plugin_Info
-    {
-        public const string GUID = "AUTHOR.MOD.GUID",
-                            NAME = "MOD NAME",
-                            VERSION = "1.0.0";
-    }
-}
